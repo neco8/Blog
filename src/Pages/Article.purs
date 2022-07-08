fcaa9b4e11037a0c978fd69a002c5717eb41e009
neco8 <shogibeshow@gmail.com>
2022-07-08 18:07:37 +0900

    add initial css

--- src/Pages/Article.purs
+++ src/Pages/Article.purs
@@ -1,15 +1,17 @@
 module Article where
 
 import Prelude
-
+import MyCSS as C
 import Data.Const (Const)
 import Halogen as H
 import Halogen.HTML as HH
 
 -- css
+css :: C.CSS
+css = do
+  pure unit
 
 -- component
-
 articleComponent :: forall m. H.Component (Const Void) Unit Void m
 articleComponent =
   H.mkComponent
